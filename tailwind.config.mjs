ad44b95a51c68c7e97943ce17a694db9decdd0d9
MichalRsa <michalrosa17@gmail.com>
2024-04-26 09:20:19 +0200

    A lot of stuff

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      "archivo-black": ["archivo-black", "sans-serif"],
+      roboto: ["roboto", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
