e18f29e82e05d15c10f390b7da6fe4b4fe978103
MichalRsa <michalrosa17@gmail.com>
2024-04-28 07:55:23 +0200

    Not that much stuff

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
     fontFamily: {
       "archivo-black": ["archivo-black", "sans-serif"],
       roboto: ["roboto", "sans-serif"],
